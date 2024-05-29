eec1e62d2d4311f021044cfcb7b66a6d76291daa
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2024-05-29 19:39:34 -0400

    Full Stack Skeleton Started

---
+++ client/src/main.jsx
@@ -0,0 +1,11 @@
+import React, { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <h1>Hello, world!</h1>
+    </React.StrictMode>
+)
